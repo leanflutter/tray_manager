50d6abdf41ada9541b76f137691f5a12d1708207
LiJianying <lijy91@foxmail.com>
2021-08-08 18:14:46 +0800

    Add windows support files

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <tray_manager/tray_manager_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  TrayManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("TrayManagerPlugin"));
+}
