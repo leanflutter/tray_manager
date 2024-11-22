0ec263ce880f9b4e7527a14c9aff201274c9086c
李健莹 <lijy91@foxmail.com>
2024-11-22 21:56:53 +0800

    wip: format

--- packages/tray_manager_macos/lib/tray_manager_macos.dart
+++ packages/tray_manager_macos/lib/tray_manager_macos.dart
@@ -1,4 +1,3 @@
-
 import 'tray_manager_macos_platform_interface.dart';
 
 class TrayManagerMacos {
--- packages/tray_manager_macos/lib/tray_manager_macos_method_channel.dart
+++ packages/tray_manager_macos/lib/tray_manager_macos_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelTrayManagerMacos extends TrayManagerMacosPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
