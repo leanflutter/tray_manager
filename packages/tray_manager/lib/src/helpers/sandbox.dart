da95dfae2d2d8a8ad9affcaed98ca572901fccc4
Sumedh Wale <sumwale@gmail.com>
2025-05-21 05:37:56 +0530

    fix the sandbox check to also work for docker/podman containers

--- packages/tray_manager/lib/src/helpers/sandbox.dart
+++ packages/tray_manager/lib/src/helpers/sandbox.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
-/// Returns `true` if the app is running in a sandbox, eg. Flatpak or Snap.
+/// Returns `true` if the app is running in a sandbox, eg. Flatpak, Snap, Docker, Podman.
 bool runningInSandbox() {
   return Platform.environment.containsKey('FLATPAK_ID') ||
-      Platform.environment.containsKey('SNAP');
+      Platform.environment.containsKey('SNAP') ||
+      Platform.environment.containsKey('container') ||
+      FileSystemEntity.isFileSync('/.dockerenv');
 }
